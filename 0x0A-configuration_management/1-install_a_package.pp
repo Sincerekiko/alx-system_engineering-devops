6e28adfe2e5f5c4c1e0e808f17357b362a094292
Sincerekiko <sincerekiko@gmail.com>
2024-03-24 16:43:31 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
